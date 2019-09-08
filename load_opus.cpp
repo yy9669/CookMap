b1618e8ee3107ccdb66c6a35b855438628e72c21
Jim McCann <ix@tchow.com>
2019-09-08 02:24:35 -0400

    overly clever attenuation? perhaps. But downmixing is certainly important.

--- load_opus.cpp
+++ load_opus.cpp
@@ -4,6 +4,7 @@
 
 #include <cassert>
 #include <memory>
+#include <cmath>
 
 void load_opus(std::string const &filename, std::vector< float > *data_) {
 	assert(data_);
@@ -25,7 +26,11 @@ void load_opus(std::string const &filename, std::vector< float > *data_) {
 		int ret = op_read_float_stereo(op.get(), pcm.data(), pcm.size());
 		if (ret >= 0) {
 			//positive return values are the number of samples read per channel; copy into data:
-			data.insert(data.end(), pcm.begin(), pcm.begin() + 2*ret);
+			data.reserve(data.size() + ret);
+			constexpr float const InvSqrt2 = std::sqrt(2.0f); //attenuating by 1.0f / sqrt(2.0f) because things equal-power panned center are this much lounder than unity.
+			for (uint32_t i = 0; i < uint32_t(ret); ++i) {
+				data.emplace_back((pcm[2*i] + pcm[2*i+1]) * InvSqrt2);
+			}
 			if (ret == 0) break;
 		} else {
 			throw std::runtime_error("opusfile read error " + std::to_string(ret) + " reading \"" + filename + "\".");
