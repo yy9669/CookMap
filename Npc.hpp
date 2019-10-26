702c259c2a5fd4d39848f374581fabf9982b8241
Haomin⭐️⭐️⭐️ <haominw@andrew.cmu.edu>
2019-10-25 21:40:53 -0400

    10252140

--- Npc.hpp
+++ Npc.hpp
@@ -16,5 +16,5 @@ public:
 	virtual ~Npc() {}
 
     npc_type type;
-    glm::vec2 position, radius = glm::vec2(50.0f, 90.0f), velocity = glm::vec2(0.0f, 0.0f);
+    glm::vec2 position, radius = glm::vec2(80.0f, 120.0f), velocity = glm::vec2(0.0f, 0.0f);
 };
