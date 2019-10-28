e4c4acfdff60f38c8d9f776684ea91da44d97daf
junjiewa <junjiewa@andrew.cmu.edu>
2019-10-28 19:25:59 -0400

    add npc motion

--- Npc.hpp
+++ Npc.hpp
@@ -12,10 +12,13 @@ public:
     Npc(npc_type type_, float x, float y) {
         type = type_;
         position = glm::vec2(x, y);
+        init_position = glm::vec2(x, y);
     }
 	virtual ~Npc() {}
 
     npc_type type;
-    glm::vec2 position, radius = glm::vec2(80.0f, 120.0f), velocity = glm::vec2(0.0f, 0.0f);
-    bool eat=false;
+    glm::vec2 position, init_position;
+    glm::vec2 radius = glm::vec2(80.0f, 120.0f);
+    glm::vec2 velocity = glm::vec2(0.0f, 60.0f);
+    bool eat = false;
 };
