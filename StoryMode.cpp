6bc4fbcb27bbf38326168921b8e8a567619f0657
Haomin⭐️⭐️⭐️ <haominw@andrew.cmu.edu>
2019-11-11 22:12:12 -0500

    11112212

--- StoryMode.cpp
+++ StoryMode.cpp
@@ -213,7 +213,7 @@ bool load_map_file(const string& filename, StoryMode* mode) {
                 case '!':
                     part = new Ingredient((ingredient_type)(10));
                     break;
-                case '1': case '2': case '3':case '4': case '5': case '6':
+                case '1': case '2': case '3':case '4': case '5': case '6': case '8':
                     part = new Ingredient((ingredient_type)(s[x] - '1'));
                     break;
                 default:
